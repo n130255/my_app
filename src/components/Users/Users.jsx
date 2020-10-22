050e42901e4aeff3119fac1a9a097ba6a991a73d
Nikita <nikita.antonov255@gmail.com>
2020-10-22 23:27:20 +0300

    little fixes

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,13 +3,12 @@ import style from './user.module.css'
 import * as axios from 'axios'
 import userPhoto from '../../assets/images/user.svg'
 class Users extends React.Component{
-    constructor(props){
-        super(props);
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response =>{
-                this.props.setUsers(response.data.items);
-            });
-        }
-     
+       
+    componentDidMount(){
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response =>{
+            this.props.setUsers(response.data.items);
+        });
+    }
     render(){
         return <div>
         {
